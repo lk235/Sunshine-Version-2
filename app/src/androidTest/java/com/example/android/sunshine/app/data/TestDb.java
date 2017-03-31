f7299eea034ce5cbbfee88dffbbe0135a4ee5514
lk235 <cool.lk235@cloud.com>
2017-03-31 23:16:34 +0800

    test Location Table

--- app/src/androidTest/java/com/example/android/sunshine/app/data/TestDb.java
+++ app/src/androidTest/java/com/example/android/sunshine/app/data/TestDb.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.sunshine.app.data;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
@@ -112,21 +113,39 @@ public class TestDb extends AndroidTestCase {
     */
     public void testLocationTable() {
         // First step: Get reference to writable database
+        WeatherDbHelper weatherDbHelper = new WeatherDbHelper(mContext);
+        SQLiteDatabase database = weatherDbHelper.getWritableDatabase();
 
         // Create ContentValues of what you want to insert
+        ContentValues contentValues = TestUtilities.createNorthPoleLocationValues();
         // (you can use the createNorthPoleLocationValues if you wish)
 
         // Insert ContentValues into database and get a row ID back
+        Long LocationRowId;
+        LocationRowId = database.insert(WeatherContract.LocationEntry.TABLE_NAME, null, contentValues);
+        assertTrue(LocationRowId != -1);
 
         // Query the database and receive a Cursor back
+        Cursor cursor = database.query(WeatherContract.LocationEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null);
 
         // Move the cursor to a valid database row
+        assertTrue("Error: NO Records returned from location query", cursor.moveToFirst());
 
         // Validate data in resulting Cursor with the original ContentValues
+        TestUtilities.validateCurrentRecord("Error: Location Query Validation Failed", cursor, contentValues);
+        assertFalse("Error: More than one Record returned from Location query", cursor.moveToNext());
         // (you can use the validateCurrentRecord function in TestUtilities to validate the
         // query if you like)
 
         // Finally, close the cursor and database
+        cursor.close();
+        database.close();
 
     }
 
